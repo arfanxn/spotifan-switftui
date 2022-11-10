d9c2d728b06ce047f7a5b4b4b40786f6b1cfe314
arfanxn <arfan2173@gmail.com>
2022-11-10 15:13:55 +0700

    remove useless stuff

--- Spotifan/Views/HomeView.swift
+++ Spotifan/Views/HomeView.swift
@@ -9,12 +9,6 @@ import SwiftUI
 
 struct HomeView: View {
 	
-	@Namespace private var animation
-	
-	@State private var verticalScrollOffset = CGFloat(0)
-	@State private var navbarOffset = CGPoint(x: 0, y: 0) ;
-	@State private var isNavbarDisappear = false
-		
 	var body: some View {
 		HomeNavbar(title: "Good Night", content: AnyView(
 			Text("Hello")
