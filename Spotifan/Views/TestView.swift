058e24b7eecb7c683014af938ffd177b935dfd90
arfanxn <arfan2173@gmail.com>
2022-11-18 17:47:46 +0700

    reset test view

--- Spotifan/Views/TestView.swift
+++ Spotifan/Views/TestView.swift
@@ -10,74 +10,15 @@ import Request
 import Json
 
 class TestViewModel : ObservableObject {
-    @Published var navbarRect : CGRect = .zero;
-    @Published var contentRect : CGRect = .zero;
-    @Published var showNavbar : Bool = true ;
 }
 
 struct TestView: View {
     @StateObject var vm : TestViewModel ;
     
     var body: some View {
-        NavigationStack{
-            ZStack{
-                ScrollView{
-                    ZStack{
-                        VStack{
-                            self.top
-                            Spacer()
-                        }
-                        .zIndex(1)
-                        
-                        LazyVStack(spacing: 20){
-                            ForEach(1..<70) { index in
-                                Rectangle()
-                                    .frame(maxWidth: .infinity)
-                                    .frame(height: 50)
-                                    .foregroundColor(.mint)
-                                    .overlay {
-                                        Text("\(index)")
-                                    }
-                            }
-                        }
-                    }
-                }
-                .clipped()
-                self.bottom
-            }
-        }
+        EmptyView()
     }
-    
-    
-    var top : some View {
-        VStack{
-            HStack{
-                ForEach(1..<4) { index in
-                    Text("\(index)")
-                }
-            }
-            .padding(.all)
-            .frame(maxWidth: .infinity)
-            .background(Color.yellow)
-            
-            Spacer()
-        }
-    }
-    
-    var bottom : some View {
-        VStack{
-            Spacer()
-            
-            HStack{
-                ForEach(1..<4) { index in
-                    Text("\(index)")
-                }
-            }
-            .padding(.all)
-            .frame(maxWidth: .infinity)
-            .background(Color.black)
-        }
-    }
+
     
 }
 
